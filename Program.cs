76677592250ad2001b41cbbf839af2fddc00b94b
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-07 16:17:29 +0300

    ex 3 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 4;
+        int[] result = new int[num + 1];
+        Parallel.For(1, num + 1, x => {
+            result[x] = x;
+        });
+        int factorial = 1;
+        for (int y = 1; y <= num; y++)
+        {
+            factorial *= result[y];
+        }
+        Console.WriteLine($"Factorial {num} - {factorial}");
+
+
+        Task<int> Count = Task.Run(() =>
+            {
+                int a = factorial.ToString().Length;
+                return a;
+            });
+
+        Task<int> Summa = Task.Run(() =>
+        {
+            int b = factorial.ToString().ToCharArray().Sum(i=>i-'0');
+            return b;
+        });
+
+        Task.WaitAll(Count, Summa);
+
+
+        Console.WriteLine($"The number of digits in the number - {Count.Result}");
+        Console.WriteLine($"Sum of all digits - {Summa.Result}");
+    }
+}
