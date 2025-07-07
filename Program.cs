1b152c45d398d032f73ceb9d99385b166262ac75
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-07 16:06:25 +0300

    ex 2 was done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 4;
+        int[] result = new int[num +1];
+        Parallel.For(1, num + 1, x => {
+            result[x] = x;
+        });
+        int factorial = 1;
+        for(int y = 1; y<= num; y++)
+        {
+            factorial *= result[y];
+        }
+        Console.WriteLine($"Factorial {num} - {factorial}");
+    }
+}
